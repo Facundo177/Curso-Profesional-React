cb9180a9294f8de196a225112a8978330f093831
Facundo177 <facundogaitansegura@gmail.com>
2026-03-21 17:07:36 -0300

    Manejo de forma correcta, con estados, del evento search en la navbar

--- ticketmaster-events/src/components/Navbar/index.jsx
+++ ticketmaster-events/src/components/Navbar/index.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 const Navbar = () => {
+  const [search, setSearch] = useState('');
+
+  const handleInputChange = (event) => {
+    setSearch(event.target.value);
+  };
+
   return (
     <div>
-      <p>Navbar</p>
-      <input placeholder="Busca tu evento favorito" />
+      <p>Mi boletera</p>
+      <input
+      placeholder="Busca tu evento favorito"
+      onChange={handleInputChange}
+      value={search}
+      />
     </div>
   );
 };
