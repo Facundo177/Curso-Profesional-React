a2a135ab10bcd708fbad6311e2a10075c4044112
Facundo177 <facundogaitansegura@gmail.com>
2026-03-21 22:20:34 -0300

    Form con ingreso de datos, manejado de forma básica sin librerias externas.

--- ticketmaster-events/src/App.jsx
+++ ticketmaster-events/src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 import Navbar from './components/Navbar';
 import Events from './components/Events';
+import SignupForm from './components/SignupForm';
 import './App.css'
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
-  
+
   const handleNavbarSearch = (term) => {
     setSearchTerm(term);
   };
@@ -14,6 +15,7 @@ function App() {
     <>
       <Navbar onSearch={handleNavbarSearch}/>
       <Events searchTerm={searchTerm}/>
+      <SignupForm />
     </>
   )
 }
