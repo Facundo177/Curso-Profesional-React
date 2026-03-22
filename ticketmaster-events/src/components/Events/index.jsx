01ea47de0a38b558368d16e878068dc80fe0c254
Facundo177 <facundogaitansegura@gmail.com>
2026-03-22 15:20:18 -0300

    Actualización del useEventsData y Eventos con más información, simulando la consulta a una API (por el momento solo es un timeout que indica que está cargando), haciendo uso del useEffect

--- ticketmaster-events/src/components/Events/index.jsx
+++ ticketmaster-events/src/components/Events/index.jsx
@@ -3,7 +3,7 @@ import EventItem from "./components/EventItem";
 
 const Events = ({searchTerm}) => {
 
-  const { events } = useEventsData();
+  const { events, isLoading, error } = useEventsData();
 
   const handleEventItemClick = (id) => {
     console.log('evento clickeado: ', id);
@@ -27,6 +27,13 @@ const Events = ({searchTerm}) => {
         />
       ));
   };
+
+  if (error) {
+    return <div>Ha ocurrido un error</div>;
+  }
+  if (isLoading) {
+    return <div>Cargando resultados...</div>
+  }
 
   return (
     <div>
